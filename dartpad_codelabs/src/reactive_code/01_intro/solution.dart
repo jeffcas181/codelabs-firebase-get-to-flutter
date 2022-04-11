3699e28237d519930c4a4af8c2ceaf8649811613
Jeff Castillo <castillojeff181@gmail.com>
2022-04-11 20:15:12 +0530

    Added introduction step

---
+++ dartpad_codelabs/src/reactive_code/01_intro/solution.dart
@@ -0,0 +1,102 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      title: 'Flutter Demo',
+      home: const CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({Key? key}) : super(key: key);
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _count = 0;
+
+  void increment() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      _count--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('setState()'),
+      ),
+      body: Center(
+        child: Container(
+          color:
+              Colors.primaries[math.Random().nextInt(Colors.primaries.length)],
+          child: Column(
+            children: [
+              CounterBody(
+                count: _count,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.grey,
+            child: const Icon(Icons.exposure_minus_1),
+            onPressed: decrement,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.red,
+            child: const Icon(Icons.exposure_plus_1),
+            onPressed: increment,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterBody extends StatelessWidget {
+  final int count;
+
+  const CounterBody({Key? key, required this.count}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(48),
+        color: Colors.primaries[math.Random().nextInt(Colors.primaries.length)],
+        child: Text(
+          count.toString(),
+          textAlign: TextAlign.center,
+        ));
+  }
+}
